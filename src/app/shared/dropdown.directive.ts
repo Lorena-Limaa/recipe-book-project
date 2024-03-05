7605346095ef13f2ee07e41b9168cc438684c547
Lorena <lorena.limaa.lima@gmail.com>
2024-03-05 19:09:52 -0300

    Implementing a RecipeService & ShoppingListService and also adding logic to the Recipe Service.

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -8,7 +8,7 @@ import {
     selector: '[appDropdown]'
 })
 export class DropdownDirective {
-    @HostBinding('class.open') isOpen = false; 
+    @HostBinding('class.open') isOpen = false;
 
     @HostListener('click') toggleOpen() {
         this.isOpen = !this.isOpen;
