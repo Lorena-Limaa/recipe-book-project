0e52a767312b974bd75aa0c4a4b49f1812bfd3af
Lorena <lorena.limaa.lima@gmail.com>
2024-03-07 15:25:43 -0300

    Adding logic to the ShoppingListService and adding ingredients from the recipe to the shopping list.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,24 +1,41 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
 
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 
+@Injectable()
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
         new Recipe(
-          'A Test Recipe', 
-          'This is simply a test', 
-          'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505'
+          'Tasty Schnitzel', 
+          'A super-tasty Schnitzel - just awesome!', 
+          'https://www.smalltownwoman.com/wp-content/uploads/2022/09/Chicken-Schnitzel-Recipe-Card-1.jpg',
+          [
+            new Ingredient('Meat', 1),
+            new Ingredient('French Fries', 20)
+          ]
         ),
         new Recipe(
-          'Another Test Recipe', 
-          'This is simply a test', 
-          'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505'
+          'Big Fat Burger', 
+          'What else you need to say?', 
+          'https://moinhoglobo.com.br/wp-content/uploads/2019/05/16-hamburguer.jpeg',
+          [
+            new Ingredient('Buns', 2),
+            new Ingredient('Meat', 1)
+          ]
         )
     ];
 
+    constructor(private slService: ShoppingListService) {}
+
     getRecipes() {
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+      this.slService.addIngredients(ingredients);
     }
 }
